51c2166c24d646eb2a9d4746a6c749eb76ead490
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-08-11 15:40:13 +0000

    *** empty log message ***

---
+++ plexus-container-default.old/src/java/org/codehaus/plexus/component/repository/ComponentProfile.java
@@ -0,0 +1,58 @@
+package org.codehaus.plexus.component.repository;
+
+import org.codehaus.plexus.component.factory.ComponentFactory;
+import org.codehaus.plexus.component.manager.ComponentManager;
+import org.codehaus.plexus.lifecycle.LifecycleHandler;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id$
+ */
+public class ComponentProfile
+{
+    /** Component Factory. */
+    private ComponentFactory componentFactory;
+
+    /** Lifecycle Handler. */
+    private LifecycleHandler lifecycleHandler;
+
+    /** Component Manager. */
+    private ComponentManager componentManager;
+
+    // ----------------------------------------------------------------------
+    // Accessors
+    // ----------------------------------------------------------------------
+
+    public ComponentFactory getComponentFactory()
+    {
+        return componentFactory;
+    }
+
+    public void setComponentFactory( ComponentFactory componentFactory )
+    {
+        this.componentFactory = componentFactory;
+    }
+
+    public LifecycleHandler getLifecycleHandler()
+    {
+        return lifecycleHandler;
+    }
+
+    public void setLifecycleHandler( LifecycleHandler lifecycleHandler )
+    {
+        this.lifecycleHandler = lifecycleHandler;
+    }
+
+    public ComponentManager getComponentManager()
+    {
+        return componentManager;
+    }
+
+    public void setComponentManager( ComponentManager componentManager )
+    {
+        this.componentManager = componentManager;
+    }
+}
