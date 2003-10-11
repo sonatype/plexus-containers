d75e2bd5e913d4b6aad3886410adcfd4a66bca45
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-10-11 00:09:38 +0000

    o exception to throw during component composition problems.

---
+++ plexus-container-default.old/src/java/org/codehaus/plexus/component/composition/CompositionException.java
@@ -0,0 +1,17 @@
+package org.codehaus.plexus.component.composition;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id$
+ */
+public class CompositionException
+    extends Exception
+{
+    public CompositionException( String s )
+    {
+        super( s );
+    }
+}
