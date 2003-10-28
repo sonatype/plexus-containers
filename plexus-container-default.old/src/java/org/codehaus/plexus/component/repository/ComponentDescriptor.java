a63c252ef73dbe16362cbbc53a9371fc5a24e84f
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-10-28 23:04:51 +0000

    o optimizo importo

--- plexus-container-default.old/src/java/org/codehaus/plexus/component/repository/ComponentDescriptor.java
+++ plexus-container-default.old/src/java/org/codehaus/plexus/component/repository/ComponentDescriptor.java
@@ -2,10 +2,8 @@ package org.codehaus.plexus.component.repository;
 
 import org.apache.avalon.framework.configuration.Configuration;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 
 /** Component instantiation description.
  *
