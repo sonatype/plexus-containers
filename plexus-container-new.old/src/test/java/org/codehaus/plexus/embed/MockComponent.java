f73618e75fb079af805590a41c39379d8d2ea62c
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-10-30 16:23:32 +0000

    o adding context test method

--- plexus-container-new.old/src/test/java/org/codehaus/plexus/embed/MockComponent.java
+++ plexus-container-new.old/src/test/java/org/codehaus/plexus/embed/MockComponent.java
@@ -1,14 +1,35 @@
 package org.codehaus.plexus.embed;
 
+import org.apache.avalon.framework.context.Contextualizable;
+import org.apache.avalon.framework.context.ContextException;
+import org.apache.avalon.framework.context.Context;
+
 /**
  * @author  Ben Walding
  * @version $Id$
  */
 public class MockComponent
+    implements Contextualizable
 {
     public static final String ROLE = MockComponent.class.getName();
-    
-    public String toString() {
+
+    public String toString()
+    {
         return "I AM MOCKCOMPONENT";
+    }
+
+    private String foo;
+
+    public String getFoo()
+    {
+        return foo;
+    }
+
+    /** @see Contextualizable#contextualize */
+    public void contextualize( Context context )
+        throws ContextException
+    {
+        foo = (String) context.get( "foo" );
+
     }
 }
