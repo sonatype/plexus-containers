22395c900ccd0dc08e0b4e27702e1514edfe84ee
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-11-01 16:45:07 +0000

    o more tests for configuration

--- plexus-container-default.old/src/test/org/codehaus/plexus/configuration/CascadingConfigurationTest.java
+++ plexus-container-default.old/src/test/org/codehaus/plexus/configuration/CascadingConfigurationTest.java
@@ -15,30 +15,47 @@ import java.io.StringReader;
 public class CascadingConfigurationTest
     extends TestCase
 {
+    private XmlPullConfigurationBuilder cb;
+
+    private String baseXml;
+
+    private String parentXml;
+
+    private Configuration base;
+
+    private Configuration parent;
+
     public CascadingConfigurationTest( String s )
     {
         super( s );
     }
 
+    public void setUp()
+        throws Exception
+    {
+        cb = new XmlPullConfigurationBuilder();
+
+        baseXml = "<conf>" +
+                    "<type default='foo'>jason</type>" +
+                    "<name>jason</name>" +
+                    "<number>0</number>" +
+                    "<boolean>true</boolean>" +
+                  "</conf>";
+
+        parentXml = "<conf>" +
+                      "<type default='bar'>jason</type>" +
+                      "<occupation>procrastinator</occupation>" +
+                      "<foo a1='1' a2='2' number='0'>bar</foo>" +
+                    "</conf>";
+
+        base = cb.parse( new StringReader( baseXml ) );
+
+        parent = cb.parse( new StringReader( parentXml ) );
+    }
+
     public void testSimpleConfigurationCascading()
         throws Exception
     {
-        XmlPullConfigurationBuilder cb = new XmlPullConfigurationBuilder();
-
-        String s0 = "<conf>" +
-                      "<type default='foo'>jason</type>" +
-                      "<name>jason</name>" +
-                    "</conf>";
-
-        String s1 = "<conf>" +
-                      "<type default='bar'>jason</type>" +
-                      "<occupation>procrastinator</occupation>" +
-                    "</conf>";
-
-        Configuration base = cb.parse( new StringReader( s0 ) );
-
-        Configuration parent = cb.parse( new StringReader( s1 ) );
-
         CascadingConfiguration cc = new CascadingConfiguration( base, parent );
 
         // Take a value from the base.
@@ -50,5 +67,56 @@ public class CascadingConfigurationTest
         // We want the 'default' attribute from the base, which effectively overrides
         // the 'default' attribute in the parent configuration.
         assertEquals( "foo", cc.getChild( "type" ).getAttribute( "default" ) );
+
+        assertEquals( 0, cc.getChild( "number" ).getValueAsInteger() );
+
+        assertEquals( 0, cc.getChild( "number" ).getValueAsLong() );
+
+        assertEquals( new Float( 0 ), new Float( cc.getChild( "number" ).getValueAsFloat() ) );
+
+        assertTrue( cc.getChild( "boolean" ).getValueAsBoolean() );
+
+        assertTrue( cc.getChild( "non-existent-boolean" ).getValueAsBoolean( true ) );
+
+        assertNotNull( cc.getChild( "foo" ).getAttributeNames() );
+
+        assertEquals( 3, cc.getChild( "foo" ).getAttributeNames().length );
+
+        // Create a new configuration.
+        Configuration c = cc.getChild( "new", true );
+
+        assertNotNull( c );
+
+        assertEquals( 0, cc.getChild( "foo" ).getAttributeAsInteger( "number" ) );
+
+        assertEquals( 0, cc.getChild( "foo" ).getAttributeAsLong( "number" ) );
+
+        assertEquals( new Float( 0 ), new Float( cc.getChild( "foo" ).getAttributeAsFloat( "number" ) ) );
+    }
+
+    public void testCascadingConfigurationWithNullParent()
+        throws Exception
+    {
+        CascadingConfiguration cc = new CascadingConfiguration( base, null );
+
+        // Take a value from the base.
+        assertEquals( "jason", cc.getChild( "name" ).getValue() );
+
+        assertEquals( "conf", cc.getName() );
+
+        assertNotNull( cc.getLocation() );
+
+        assertNotNull( cc.getNamespace() );
+    }
+
+    public void testCascadingConfigurationWithNullBase()
+        throws Exception
+    {
+        CascadingConfiguration cc = new CascadingConfiguration( null, parent );
+
+        // Take a value from the parent.
+        assertEquals( "procrastinator", cc.getChild( "occupation" ).getValue() );
+
+        assertEquals( "-", cc.getName() );
     }
 }
