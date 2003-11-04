176dfb31c2d882a5e0e6b024f49710a5cfe0b294
mhw <mhw@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-11-04 16:54:16 +0000

    Remove unused imports. Correct JavaDoc.

--- plexus-container-default.old/src/test/org/codehaus/plexus/DefaultServiceH.java
+++ plexus-container-default.old/src/test/org/codehaus/plexus/DefaultServiceH.java
@@ -5,24 +5,13 @@ import org.codehaus.plexus.lifecycle.phase.Mo;
 import org.codehaus.plexus.lifecycle.phase.Miny;
 import org.codehaus.plexus.lifecycle.phase.Meeny;
 import org.codehaus.plexus.lifecycle.phase.Eeny;
-import org.apache.avalon.framework.context.Contextualizable;
-import org.apache.avalon.framework.context.Context;
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.configuration.Configurable;
-import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.avalon.framework.service.Serviceable;
-import org.apache.avalon.framework.activity.Initializable;
-import org.apache.avalon.framework.activity.Startable;
-import org.apache.avalon.framework.logger.Logger;
 
-/** This component implements all the start phases:
+/** This component implements the custom lifecycle defined by the phases
  *
- *  LogEnabled
- *  Contexualize
- *  Serviceable
- *  Configurable
- *  Initializable
- *  Startable
+ *  Eeny
+ *  Meeny
+ *  Miny
+ *  Mo
  *
  */
 public class DefaultServiceH
@@ -35,7 +24,7 @@ public class DefaultServiceH
     boolean mo;
 
     // ----------------------------------------------------------------------
-    // Lifecylce Management
+    // Lifecycle Management
     // ----------------------------------------------------------------------
 
     public void eeny()
