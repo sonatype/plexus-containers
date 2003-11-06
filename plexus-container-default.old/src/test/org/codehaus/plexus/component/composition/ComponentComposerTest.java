bc8b7dc302464775fcb21578e9d8abd6454c1a99
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-11-06 22:35:42 +0000

    *** empty log message ***

--- plexus-container-default.old/src/test/org/codehaus/plexus/component/composition/ComponentComposerTest.java
+++ plexus-container-default.old/src/test/org/codehaus/plexus/component/composition/ComponentComposerTest.java
@@ -2,11 +2,9 @@ package org.codehaus.plexus.component.composition;
 
 import junit.framework.TestCase;
 
-import java.lang.reflect.Field;
-
 /**
  *
- * 
+ *
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  *
  * @version $Id$
@@ -26,5 +24,39 @@ public class ComponentComposerTest
         ComponentB b = a.getComponentB();
 
         assertNotNull( b );
+    }
+
+    public void testComponentAssignmentWithIncompatibleField()
+        throws Exception
+    {
+        DefaultComponentComposer composer = new DefaultComponentComposer();
+
+        ComponentA a = new ComponentA();
+
+        try
+        {
+            composer.assignComponent( "foo", a );
+        }
+        catch ( CompositionException e )
+        {
+            assertEquals( "No field which is compatible in target object.", e.getMessage() );
+        }
+    }
+
+    public void testComponentAssignmentWithNullTarget()
+        throws Exception
+    {
+        DefaultComponentComposer composer = new DefaultComponentComposer();
+
+        ComponentA a = null;
+
+        try
+        {
+            composer.assignComponent( new ComponentB(), a );
+        }
+        catch ( CompositionException e )
+        {
+            assertEquals( "Target object is null.", e.getMessage() );
+        }
     }
 }
