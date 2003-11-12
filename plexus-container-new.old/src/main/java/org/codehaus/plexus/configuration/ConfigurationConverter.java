1b1d25e709912a34064298b5d9a9df60347ee485
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-11-12 22:39:16 +0000

    o A configuration converter which will leave Configuations intact and just   pass them though to the underlying object instead of trying to unmarshall   them. I just added a peek() method to my ConfigurationReader but it might be   useful in xstream generally.

---
+++ plexus-container-new.old/src/main/java/org/codehaus/plexus/configuration/ConfigurationConverter.java
@@ -0,0 +1,37 @@
+package org.codehaus.plexus.configuration;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.ConverterLookup;
+import com.thoughtworks.xstream.objecttree.ObjectTree;
+import com.thoughtworks.xstream.xml.XMLReader;
+import com.thoughtworks.xstream.xml.XMLWriter;
+import org.apache.avalon.framework.configuration.Configuration;
+
+public class ConfigurationConverter
+    implements Converter
+{
+    public boolean canConvert( Class type )
+    {
+        return Configuration.class.isAssignableFrom( type );
+    }
+
+    public void toXML( ObjectTree objectGraph, XMLWriter xmlWriter, ConverterLookup converterLookup )
+    {
+        // When I create the configuration serializer I'll use that here.
+    }
+
+    public void fromXML( ObjectTree objectGraph, XMLReader xmlReader, ConverterLookup converterLookup, Class requiredType )
+    {
+        ConfigurationReader reader = (ConfigurationReader) xmlReader;
+
+        PlexusConfiguration configuration = (PlexusConfiguration) reader.peek();
+
+        while ( xmlReader.nextChild() )
+        {
+            xmlReader.pop();
+        }
+
+        objectGraph.set( configuration );
+    }
+
+}
