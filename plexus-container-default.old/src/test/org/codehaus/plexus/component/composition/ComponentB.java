dcc5246f07f852d8bc91bc873557c95f9c86dc76
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-12-19 16:42:46 +0000

    o adding pieces in that IDEA missed.

---
+++ plexus-container-default.old/src/test/org/codehaus/plexus/component/composition/ComponentB.java
@@ -0,0 +1,12 @@
+package org.codehaus.plexus.component.composition;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id$
+ */
+public interface ComponentB
+{
+}
