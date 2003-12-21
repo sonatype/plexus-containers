86325fb8fa94787dde7e8ffbb306fff6f67eddac
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-12-21 21:28:35 +0000

    o adding addition tests that are more complicated.

--- plexus-container-default.old/src/test/org/codehaus/plexus/component/composition/CompositionResolverTest.java
+++ plexus-container-default.old/src/test/org/codehaus/plexus/component/composition/CompositionResolverTest.java
@@ -60,4 +60,93 @@ public class CompositionResolverTest
 
         assertEquals( 2, dependencies.size() );
     }
+
+    // ------------------------------------------------------------------------
+    //
+    //     +-------+           +-------+
+    //     |  c1   | --------> |  c2   |
+    //     +-------+           +-------+
+    //         |
+    //         |
+    //         V
+    //     +-------+           +-------+
+    //     |  c3   | --------> |  c4   |
+    //     +-------+           +-------+
+    //         |
+    //         |
+    //         V
+    //     +-------+
+    //     |  c5   |
+    //     +-------+
+    //
+    // ------------------------------------------------------------------------
+    public void testComplexComponentResolution()
+        throws Exception
+    {
+        String cc1 =
+            "<component>" +
+            "  <role>c1</role>" +
+            "  <requirements>" +
+            "    <requirement>c2</requirement>" +
+            "    <requirement>c3</requirement>" +
+            "  </requirements>" +
+            "</component>";
+
+        String cc2 =
+            "<component>" +
+            "  <role>c2</role>" +
+            "</component>";
+
+        String cc3 =
+            "<component>" +
+            "  <role>c3</role>" +
+            "  <requirements>" +
+            "    <requirement>c4</requirement>" +
+            "    <requirement>c5</requirement>" +
+            "  </requirements>" +
+            "</component>";
+
+        String cc4 =
+            "<component>" +
+            "  <role>c4</role>" +
+            "</component>";
+
+        String cc5 =
+            "<component>" +
+            "  <role>c5</role>" +
+            "</component>";
+
+
+        CompositionResolver cr = new CompositionResolver();
+
+        ComponentDescriptor c1 = PlexusTools.buildComponentDescriptor( cc1 );
+
+        ComponentDescriptor c2 = PlexusTools.buildComponentDescriptor( cc2 );
+
+        ComponentDescriptor c3 = PlexusTools.buildComponentDescriptor( cc3 );
+
+        ComponentDescriptor c4 = PlexusTools.buildComponentDescriptor( cc4 );
+
+        ComponentDescriptor c5 = PlexusTools.buildComponentDescriptor( cc5 );
+
+        cr.addComponentDescriptor( c1 );
+
+        cr.addComponentDescriptor( c2 );
+
+        cr.addComponentDescriptor( c3 );
+
+        cr.addComponentDescriptor( c4 );
+
+        cr.addComponentDescriptor( c5 );
+
+        List dependencies = cr.getComponentDependencies( c1.getComponentKey() );
+
+        assertEquals( 4, dependencies.size() );
+
+        // c5 must come before c3
+        assertTrue( dependencies.indexOf( "c5" ) < dependencies.indexOf( "c3" ) );
+
+        // c4 must come before c3
+        assertTrue( dependencies.indexOf( "c4" ) < dependencies.indexOf( "c3" ) );
+    }
 }
