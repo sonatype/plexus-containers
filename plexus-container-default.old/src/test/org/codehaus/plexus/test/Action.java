b90c40efdf16e6db71637e827103d09c726d3361
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2003-12-30 04:28:51 +0000

    *** empty log message ***

---
+++ plexus-container-default.old/src/test/org/codehaus/plexus/test/Action.java
@@ -0,0 +1,12 @@
+package org.codehaus.plexus.test;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id$
+ */
+public interface Action
+{
+}
---
+++ plexus-container-new.old/src/test/java/org/codehaus/plexus/test/AddUserAction.java
@@ -0,0 +1,13 @@
+package org.codehaus.plexus.test;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id$
+ */
+public class AddUserAction
+    implements Action
+{
+}
