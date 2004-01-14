73f7594f98f0fd3df939f5c450108af6f46cf27e
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-01-14 01:03:35 +0000

    o moving console logger

---
+++ plexus-container-default.old/src/java/org/codehaus/plexus/logging/console/ConsoleLoggerManager.java
@@ -0,0 +1,103 @@
+package org.codehaus.plexus.logging.console;
+
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
+import org.codehaus.plexus.logging.LoggerManager;
+import org.codehaus.plexus.logging.Logger;
+
+
+/**
+ * Sample configuration.
+ *
+ * <pre>
+ * <logging>
+ *   <implementation>org.codehaus.plexus.logging.ConsoleLoggerManager</implementation>
+ *   <logger>
+ *     <threshold>DEBUG</threshold>
+ *   </logger>
+ * </logging>
+ * </pre>
+ */
+public class ConsoleLoggerManager
+    implements LoggerManager
+{
+    /** XML element used to start the logger configuration. */
+    private static final String LOGGER_TAG = "logger";
+
+    /** XML element used to set the threshold of the console logger. */
+    private static final String THRESHOLD_TAG = "threshold";
+
+    /** Message of this level or higher will be logged. */
+    private int thresholdLevel;
+
+    /** The console logger used by the manager. */
+    private ConsoleLogger consoleLogger;
+
+    public void configure( PlexusConfiguration configuration )
+    {
+        setThresholdLevel(
+            configuration
+            .getChild( LOGGER_TAG )
+            .getChild( THRESHOLD_TAG )
+            .getValue( "info" )
+            .trim()
+            .toLowerCase() );
+    }
+
+    public void initialize()
+        throws Exception
+    {
+        consoleLogger = new ConsoleLogger( thresholdLevel );
+    }
+
+    public void start()
+        throws Exception
+    {
+    }
+
+    public void stop()
+    {
+    }
+
+    public Logger getRootLogger()
+    {
+        return consoleLogger;
+    }
+
+    public Logger getLogger( String name )
+    {
+        return consoleLogger.getChildLogger( name );
+    }
+
+    /**
+     * Sets the threshold for the console logger created by this
+     * manager.
+     *
+     * @param text The threshold level specified as a string which can
+     * be one of the following: debug, info, warn, error, fatal,
+     * disabled.
+     */
+    private void setThresholdLevel( String text )
+    {
+        if ( text.equals( "debug" ) )
+        {
+            thresholdLevel = ConsoleLogger.LEVEL_DEBUG;
+        }
+        else if ( text.equals( "info" ) )
+        {
+            thresholdLevel = ConsoleLogger.LEVEL_INFO;
+        }
+        else if ( text.equals( "warn" ) )
+        {
+            thresholdLevel = ConsoleLogger.LEVEL_WARN;
+        }
+        else if ( text.equals( "error" ) )
+        {
+            thresholdLevel = ConsoleLogger.LEVEL_ERROR;
+        }
+        else if ( text.equals( "fatal" ) )
+        {
+            thresholdLevel = ConsoleLogger.LEVEL_FATAL;
+        }
+    }
+}
