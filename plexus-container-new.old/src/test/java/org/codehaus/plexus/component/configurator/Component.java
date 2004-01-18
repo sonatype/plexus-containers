15085ef95b1ebb7ecde8b15330b4f74824845db6
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-01-18 07:18:18 +0000

    *** empty log message ***

---
+++ plexus-container-new.old/src/test/java/org/codehaus/plexus/component/configurator/Component.java
@@ -0,0 +1,12 @@
+package org.codehaus.plexus.component.configurator;
+
+/**
+ *
+ * 
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ *
+ * @version $Id$
+ */
+public interface Component
+{
+}
