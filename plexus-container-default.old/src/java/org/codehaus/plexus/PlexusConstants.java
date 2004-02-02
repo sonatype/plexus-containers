12662b196c3873455b0fba650c18fbc921d4be76
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-02-02 00:03:06 +0000

    o only three of these are being used, the rest are vestiges.

--- plexus-container-default.old/src/java/org/codehaus/plexus/PlexusConstants.java
+++ plexus-container-default.old/src/java/org/codehaus/plexus/PlexusConstants.java
@@ -2,68 +2,9 @@ package org.codehaus.plexus;
 
 public abstract class PlexusConstants
 {
-    /** Default selector tag. */
-    public static final String DEFAULT_SELECTOR = "default-selector";
-
-    /** Selector tag. */
-    public static final String SELECTOR = "selector";
-
-    /** Selectors tag. */
-    public static final String SELECTORS = "selectors";
-
-    /** Default selector implementation tag. */
-    public static final String DEFAULT_SERVICE_SELECTOR_IMPL = "org.codehaus.plexus.component.PlexusServiceSelector";
-
-    /** Selector impl suffix tag. */
-    public static final String SELECTOR_IMPL_SUFFIX = "Selector";
-
-    /** Component tag. */
-    public static final String COMPONENT = "component";
-
-    /** Components tag. */
-    public static final String COMPONENTS = "components";
-
-    /** Id tag. */
-    public static final String ID = "id";
-
-    /** Role tag. */
-    public static final String ROLE = "role";
-
-    /** Roles tag. */
-    public static final String ROLES = "roles";
-
-    /** Implementation tag. */
-    public static final String IMPL_KEY = "implementation";
-
-    /** Configuration tag. */
-    public static final String CONFIGURATION = "configuration";
-
-    /** Key to retrieve the main PlexusContainer object from the context. **/
     public static final String PLEXUS_KEY = "plexus";
 
-    /** Key to retrieve the <code>File</code> plexus root, if any. */
-    public static final String ROOT_KEY = "plexus:root";
-
-    /** Key to retrieve the <code>File</code> plexus root,
-     if any. */
-    public static final String ROOT_URL_KEY = "plexus:root-url";
-
-    /** Key to retrieve the <code>File</code> plexus deployment root, if
-     any. */
-    public static final String DEPLOY_KEY = "plexus:deploy";
-
-    /** Key to retrieve the <code>File</code> plexus working-directory
-     root, if any. */
-    public static final String WORK_KEY = "plexus:work";
-
-    /** Key to retrieve the <code>File</code> plexus runtime root, if
-     any. */
-    public static final String RUN_KEY = "plexus:run";
-
-    /** Key to retrieve the <code>File</code> plexus runtime root, if
-     any. */
     public static final String RESOURCE_MANAGER_KEY = "plexus:resource-manager";
 
-    /** Common classloader */
     public static final String COMMON_CLASSLOADER = "common.classloader";
 }
