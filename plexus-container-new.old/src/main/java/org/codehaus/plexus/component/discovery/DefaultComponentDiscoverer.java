9707a9236870d63286c8e255a17528989d5d6df9
mhw <mhw@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-02-20 18:22:52 +0000

    Close stream when we're done with it.

--- plexus-container-new.old/src/main/java/org/codehaus/plexus/component/discovery/DefaultComponentDiscoverer.java
+++ plexus-container-new.old/src/main/java/org/codehaus/plexus/component/discovery/DefaultComponentDiscoverer.java
@@ -56,6 +56,8 @@ public class DefaultComponentDiscoverer
                     os.write( buffer, 0, read );
                 }
 
+                is.close();
+
                 String configuration = os.toString();
 
                 PlexusConfiguration[] componentConfigurations =
