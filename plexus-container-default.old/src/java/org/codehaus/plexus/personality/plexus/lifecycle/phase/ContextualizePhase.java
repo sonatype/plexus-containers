7ea3273e11adf0ea89bf5c8ededf79e75f46983e
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-02-29 20:37:42 +0000

    o get the context from the container.

--- plexus-container-default.old/src/java/org/codehaus/plexus/personality/plexus/lifecycle/phase/ContextualizePhase.java
+++ plexus-container-default.old/src/java/org/codehaus/plexus/personality/plexus/lifecycle/phase/ContextualizePhase.java
@@ -1,10 +1,8 @@
 package org.codehaus.plexus.personality.plexus.lifecycle.phase;
 
 import org.codehaus.plexus.component.manager.ComponentManager;
-import org.codehaus.plexus.component.composition.ComponentComposer;
-import org.codehaus.plexus.lifecycle.phase.AbstractPhase;
-import org.codehaus.plexus.lifecycle.LifecycleHandler;
 import org.codehaus.plexus.context.Context;
+import org.codehaus.plexus.lifecycle.phase.AbstractPhase;
 
 public class ContextualizePhase
     extends AbstractPhase
@@ -12,10 +10,10 @@ public class ContextualizePhase
     public void execute( Object object, ComponentManager manager )
         throws Exception
     {
-        Context context = (Context) manager.getLifecycleHandler().getEntities().get( LifecycleHandler.CONTEXT );
-
         if ( object instanceof Contextualizable )
         {
+            Context context = manager.getContainer().getContext();
+
             ( (Contextualizable) object ).contextualize( context );
         }
     }
