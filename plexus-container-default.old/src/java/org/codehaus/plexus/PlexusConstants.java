96829140299feb23c933de8101fad7684f3531bb
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-03-05 01:19:53 +0000

    o removing unused constants.

--- plexus-container-default.old/src/java/org/codehaus/plexus/PlexusConstants.java
+++ plexus-container-default.old/src/java/org/codehaus/plexus/PlexusConstants.java
@@ -3,8 +3,4 @@ package org.codehaus.plexus;
 public abstract class PlexusConstants
 {
     public static final String PLEXUS_KEY = "plexus";
-
-    public static final String RESOURCE_MANAGER_KEY = "plexus:resource-manager";
-
-    public static final String COMMON_CLASSLOADER = "common.classloader";
 }
