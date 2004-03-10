3af6c67a311989326d6e44e4009d98ea766c24e4
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-03-10 08:44:13 +0000

    o Removed a lot of very strange abstract methods that are defined in the   LoggerManager interface.

--- plexus-container-default.old/src/java/org/codehaus/plexus/logging/AbstractLoggerManager.java
+++ plexus-container-default.old/src/java/org/codehaus/plexus/logging/AbstractLoggerManager.java
@@ -1,23 +1,9 @@
 package org.codehaus.plexus.logging;
 
-
-
-
 public abstract class AbstractLoggerManager
     implements LoggerManager
 {
-    public abstract Logger getRootLogger();
-
-    public abstract Logger getLogger( String name );
-
-    public abstract void initialize()
-        throws Exception;
-
-    public abstract void start()
-        throws Exception;
-
-    public abstract void stop()
-        throws Exception;
-
-
+    /** */
+    public AbstractLoggerManager() {        
+    }
 }
