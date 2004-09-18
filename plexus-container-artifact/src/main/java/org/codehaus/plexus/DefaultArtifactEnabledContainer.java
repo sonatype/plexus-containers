708b5ad4f19b6577a3fec0caabc0ef07befb4f04
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-09-18 01:24:45 +0000

    o adding the ability to match up dependent component realms with   components that are added. this is a crude cut at this but it   works to satisfy the requirement in maven where a mojo uses a   plexus component which itself has dependencies where some of   those dependencies are themselves components.

--- plexus-container-artifact/src/main/java/org/codehaus/plexus/DefaultArtifactEnabledContainer.java
+++ plexus-container-artifact/src/main/java/org/codehaus/plexus/DefaultArtifactEnabledContainer.java
@@ -1,10 +1,5 @@
 package org.codehaus.plexus;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
@@ -13,9 +8,13 @@ import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.codehaus.classworlds.ClassRealm;
 import org.codehaus.classworlds.ClassWorld;
-import org.codehaus.plexus.DefaultPlexusContainer;
 import org.codehaus.plexus.component.repository.ComponentDependency;
 import org.codehaus.plexus.component.repository.ComponentDescriptor;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author jdcasey
@@ -43,11 +42,15 @@ public class DefaultArtifactEnabledContainer
 
     int realmTmpId = 0;
 
-    public void addComponent( Artifact component, ArtifactResolver artifactResolver, Set remoteRepositories,
-        ArtifactRepository localRepository, ArtifactMetadataSource sourceReader, String[] artifactExcludes )
+    public void addComponent( Artifact component,
+                              ArtifactResolver artifactResolver,
+                              Set remoteRepositories,
+                              ArtifactRepository localRepository,
+                              ArtifactMetadataSource sourceReader,
+                              String[] artifactExcludes )
         throws Exception
     {
-        boolean isolatedRealm = false;
+        boolean dependencyComponentsDiscovered = false;
 
         // I need an active filter and not an excludes list here.
 
@@ -65,6 +68,7 @@ public class DefaultArtifactEnabledContainer
         realmTmpId++;
 
         ClassWorld classWorld = getClassWorld();
+
         ClassRealm tmp = classWorld.newRealm( "tmp" + realmTmpId );
 
         tmp.addConstituent( component.getFile().toURL() );
@@ -72,6 +76,26 @@ public class DefaultArtifactEnabledContainer
         List componentDescriptors = discoverComponents( tmp );
 
         classWorld.disposeRealm( "tmp" + realmTmpId );
+
+        // ----------------------------------------------------------------------
+        // Now we walk through any of the component descriptors that we find in
+        // the JAR of the component we are adding and there may be several of
+        // them.
+        //
+        // NOTE: We must still deal with components that might be found in the
+        // depenendencies of this component. For example a Maven plugin has
+        // component descriptors in the plugin JAR itself, but if it relies on
+        // components that are found within the dependent JARs then we must
+        // account for that as well.
+        //
+        // To make this easier we should probably get something back from the
+        // discovery process which has the individual component descriptors as
+        // well as a list of their dependencies. Right now this is being hacked
+        // in by repeating the list of dependencies for each component and
+        // attaching them. The indivdual list of dependencies may indeed by
+        // useful but I really only need to process them here once insofar as
+        // discovering components inside the dependencies. jvz.
+        // ----------------------------------------------------------------------
 
         for ( Iterator i = componentDescriptors.iterator(); i.hasNext(); )
         {
@@ -81,32 +105,23 @@ public class DefaultArtifactEnabledContainer
 
             ClassRealm componentRealm;
 
-            if ( isolatedRealm )
-            {
-                componentRealm = classWorld.newRealm( componentKey );
-            }
-            else
-            {
-                ClassRealm plexusRealm = getContainerRealm();
-                componentRealm = plexusRealm.createChildRealm( componentKey );
-            }
+            ClassRealm plexusRealm = getContainerRealm();
 
-            componentRealm.addConstituent( component.getFile().toURL() );
+            componentRealm = plexusRealm.createChildRealm( componentKey );
 
             if ( componentDescriptor.getComponentSetDescriptor().getDependencies() != null )
             {
                 Set artifactsToResolve = new HashSet();
 
-                for ( Iterator j = componentDescriptor.getComponentSetDescriptor().getDependencies().iterator(); j
-                    .hasNext(); )
+                for ( Iterator j = componentDescriptor.getComponentSetDescriptor().getDependencies().iterator(); j.hasNext(); )
                 {
                     ComponentDependency cd = (ComponentDependency) j.next();
 
                     artifactsToResolve.add( createArtifact( cd ) );
                 }
 
-                ArtifactResolutionResult result = artifactResolver.resolveTransitively( artifactsToResolve,
-                    remoteRepositories, localRepository, sourceReader );
+                ArtifactResolutionResult result =
+                    artifactResolver.resolveTransitively( artifactsToResolve, remoteRepositories, localRepository, sourceReader );
 
                 for ( Iterator k = result.getArtifacts().values().iterator(); k.hasNext(); )
                 {
@@ -130,6 +145,43 @@ public class DefaultArtifactEnabledContainer
                     }
                 }
             }
+
+            // ----------------------------------------------------------------------
+            // Now all the dependencies have been processed and we have a realm that
+            // is full of the components dependencies. We have already discovered
+            // components in the JAR that was passed into this method so at this
+            // point all we need to do is discover any components that may be
+            // contained within the dependencies.
+            // ----------------------------------------------------------------------
+
+            if ( !dependencyComponentsDiscovered )
+            {
+                List dependencyComponents = discoverComponents( componentRealm );
+
+                // ----------------------------------------------------------------------
+                // We have to make sure that components among the dependencies have
+                // their realms aligned with the realm of the component that was added.
+                // If this is not done then component will use the container realm which
+                // will not contain the classes required for the component because they
+                // are put in the added component's realm here.
+                // ----------------------------------------------------------------------
+
+                for ( Iterator j = dependencyComponents.iterator(); j.hasNext(); )
+                {
+                    ComponentDescriptor dcd = (ComponentDescriptor) j.next();
+
+                    addRealmAlias( dcd.getComponentKey(), componentKey );
+                }
+
+                dependencyComponentsDiscovered = true;
+            }
+
+            // ----------------------------------------------------------------------
+            // Now that the dependencies have been processed we can add the component
+            // JAR itself into the realm and everything is now ready for use.
+            // ----------------------------------------------------------------------
+
+            componentRealm.addConstituent( component.getFile().toURL() );
         }
     }
 }
