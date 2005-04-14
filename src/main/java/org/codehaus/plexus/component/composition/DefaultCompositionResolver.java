d06461f37f2f6da79fea0c3dc04bcf3723f29144
brett <brett@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-04-14 06:13:58 +0000

    don't throw away exception and handle cycles

--- src/main/java/org/codehaus/plexus/component/composition/DefaultCompositionResolver.java
+++ src/main/java/org/codehaus/plexus/component/composition/DefaultCompositionResolver.java
@@ -26,26 +26,25 @@ package org.codehaus.plexus.component.composition;
 
 import org.codehaus.plexus.component.repository.ComponentDescriptor;
 import org.codehaus.plexus.component.repository.ComponentRequirement;
+import org.codehaus.plexus.util.dag.CycleDetectedException;
 import org.codehaus.plexus.util.dag.DAG;
 
 import java.util.Iterator;
 import java.util.List;
 
 
-
 /**
- *
- *
  * @author <a href="mailto:michal.maczka@dimatics.com">Michal Maczka</a>
- *
  * @version $Id$
  */
-public class DefaultCompositionResolver implements CompositionResolver
+public class DefaultCompositionResolver
+    implements CompositionResolver
 {
     private DAG dag = new DAG();
 
 
-    public void addComponentDescriptor( final ComponentDescriptor componentDescriptor ) throws CompositionException
+    public void addComponentDescriptor( final ComponentDescriptor componentDescriptor )
+        throws CompositionException
     {
         final String componentKey = componentDescriptor.getComponentKey();
 
@@ -53,26 +52,20 @@ public class DefaultCompositionResolver implements CompositionResolver
 
         for ( final Iterator iterator = requirements.iterator(); iterator.hasNext(); )
         {
-            final ComponentRequirement requirement = ( ComponentRequirement ) iterator.next();
+            final ComponentRequirement requirement = (ComponentRequirement) iterator.next();
 
             try
             {
                 dag.addEdge( componentKey, requirement.getRole() );
             }
-            catch( Exception e)
+            catch ( CycleDetectedException e )
             {
-
-
-
+                throw new CompositionException( "Cyclic requirement detected", e );
             }
         }
-
-
-
     }
 
     /**
-     *
      * @see org.codehaus.plexus.component.composition.CompositionResolver#getRequirements(java.lang.String)
      */
     public List getRequirements( final String componentKey )
