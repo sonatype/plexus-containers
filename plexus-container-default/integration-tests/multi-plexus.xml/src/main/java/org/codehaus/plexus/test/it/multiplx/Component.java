9adc3805c72a75ba2a660cc035a9082464e2db42
jdcasey <jdcasey@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-04-18 15:56:29 +0000

    committing integration test and test-fodder component for the multiple plexus.xml support.

---
+++ plexus-container-default/integration-tests/multi-plexus.xml/src/main/java/org/codehaus/plexus/test/it/multiplx/Component.java
@@ -0,0 +1,26 @@
+package org.codehaus.plexus.test.it.multiplx;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public interface Component
+{
+    
+    public static final String ROLE = Component.class.getName();
+    
+    public String testValue(String input);
+
+}
