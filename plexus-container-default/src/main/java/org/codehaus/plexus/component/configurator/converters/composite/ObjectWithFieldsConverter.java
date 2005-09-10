f96c711bee3f65ae9511d00448bdbedc10f49c1e
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-09-10 00:06:27 +0000

    o adding ability to use setters in components

--- plexus-container-default/src/main/java/org/codehaus/plexus/component/configurator/converters/composite/ObjectWithFieldsConverter.java
+++ plexus-container-default/src/main/java/org/codehaus/plexus/component/configurator/converters/composite/ObjectWithFieldsConverter.java
@@ -26,7 +26,6 @@ package org.codehaus.plexus.component.configurator.converters.composite;
 
 import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
 import org.codehaus.plexus.component.configurator.converters.AbstractConfigurationConverter;
-import org.codehaus.plexus.component.configurator.converters.ConfigurationConverter;
 import org.codehaus.plexus.component.configurator.converters.lookup.ConverterLookup;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
