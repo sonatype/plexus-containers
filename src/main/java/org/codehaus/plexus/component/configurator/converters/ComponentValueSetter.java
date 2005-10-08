583da01e7921496e0cc33fac62b0a9cfbfed7c09
brett <brett@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-10-08 08:20:57 +0000

    updated group

--- src/main/java/org/codehaus/plexus/component/configurator/converters/ComponentValueSetter.java
+++ src/main/java/org/codehaus/plexus/component/configurator/converters/ComponentValueSetter.java
@@ -8,7 +8,6 @@ import org.codehaus.plexus.component.configurator.ConfigurationListener;
 import org.codehaus.plexus.component.configurator.converters.lookup.ConverterLookup;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
-import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
