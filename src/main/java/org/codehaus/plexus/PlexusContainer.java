73feff97f7be8784069dd6b9680d143a3390cdc7
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-05-25 17:19:32 +0000

    o adding an interface back for maven compatibility

--- src/main/java/org/codehaus/plexus/PlexusContainer.java
+++ src/main/java/org/codehaus/plexus/PlexusContainer.java
@@ -9,6 +9,7 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
 import org.codehaus.plexus.component.repository.exception.ComponentRepositoryException;
 import org.codehaus.plexus.configuration.PlexusConfigurationResourceException;
 import org.codehaus.plexus.context.Context;
+import org.codehaus.plexus.logging.LoggerManager;
 
 import java.io.File;
 import java.io.Reader;
@@ -145,4 +146,11 @@ public interface PlexusContainer
     void setReloadingEnabled( boolean reloadingEnabled );
 
     boolean isReloadingEnabled();
+
+    // ----------------------------------------------------------------------------
+    // Required for compatibility
+    // ----------------------------------------------------------------------------
+
+    // Required by maven
+    LoggerManager getLoggerManager();
 }
