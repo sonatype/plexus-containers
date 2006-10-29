9a18654057ad00efcb0f46c7772878bf43a44a00
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-10-29 14:18:28 +0000

    Merge the container-refactoring work into trunk - added a little extra work to logging to make it all work against the latest api

--- src/main/java/org/codehaus/plexus/component/discovery/ComponentDiscoveryListener.java
+++
@@ -1,8 +0,0 @@
-package org.codehaus.plexus.component.discovery;
-
-public interface ComponentDiscoveryListener
-{
-    void componentDiscovered( ComponentDiscoveryEvent event );
-
-    String getId();
-}
