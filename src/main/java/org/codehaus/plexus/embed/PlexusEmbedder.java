4966dff5e6960b9a565df198c6a6d4a4c9acf501
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-11-23 20:23:01 +0000

    o add the embedder back for backward compat

---
+++ src/main/java/org/codehaus/plexus/embed/PlexusEmbedder.java
@@ -0,0 +1,62 @@
+/* Created on Oct 7, 2004 */
+package org.codehaus.plexus.embed;
+
+import org.codehaus.classworlds.ClassWorld;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.PlexusContainerException;
+import org.codehaus.plexus.component.repository.exception.ComponentLifecycleException;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.configuration.PlexusConfigurationResourceException;
+import org.codehaus.plexus.logging.LoggerManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.util.Properties;
+
+/**
+ * @author jdcasey
+ */
+public interface PlexusEmbedder
+{
+
+    PlexusContainer getContainer();
+
+    Object lookup( String role ) throws ComponentLookupException;
+
+    Object lookup( String role, String id ) throws ComponentLookupException;
+
+    boolean hasComponent( String role );
+
+    boolean hasComponent( String role, String id );
+
+    void release( Object service )
+        throws ComponentLifecycleException;
+
+    void setClassWorld( ClassWorld classWorld );
+
+    void setConfiguration( URL configuration ) throws IOException;
+
+    void setConfiguration( Reader configuration ) throws IOException;
+
+    void addContextValue( Object key, Object value );
+
+    void setProperties( Properties properties );
+
+    void setProperties( File file );
+
+    void start( ClassWorld classWorld )
+        throws PlexusContainerException, PlexusConfigurationResourceException;
+
+    void start()
+        throws PlexusContainerException, PlexusConfigurationResourceException;
+
+    void stop();
+
+    // ----------------------------------------------------------------------
+    //
+    // ----------------------------------------------------------------------
+
+    void setLoggerManager( LoggerManager loggerManager );    
+}
---
+++ src/test/java/org/codehaus/plexus/embed/EmbedderTest.java
@@ -0,0 +1,92 @@
+package org.codehaus.plexus.embed;
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004, The Codehaus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import junit.framework.TestCase;
+
+import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * @author Ben Walding
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ */
+public class EmbedderTest
+    extends TestCase
+{
+    public void testConfigurationByURL()
+        throws Exception
+    {
+        try
+        {
+            Map context = new HashMap();
+
+            context.put( "foo", "bar" );
+
+            context.put( "property1", "value1" );
+
+            context.put( "property2", "value2" );
+
+            String configuration = getClass().getName().replace( '.', '/' ) + ".xml";
+
+            System.out.println( "configuration = " + configuration );
+
+            PlexusEmbedder embed = new Embedder( context, configuration );
+
+            Object o = embed.lookup( MockComponent.ROLE );
+
+            assertEquals( "I AM MOCKCOMPONENT", o.toString() );
+
+            assertNotNull( getClass().getResource( "/test.txt" ) );
+
+            //while ( true )
+            //{
+            // infinite loop generate an StackTrace stackOverflow 
+            // due to recreation of component for each lookup
+            // Try it by removing comments but not commit this ;-)
+
+            Object m = embed.lookup( MockComponent.ROLE );
+
+            assertEquals( "not same hashCode for singleton component", o.hashCode(), m.hashCode() );
+
+            Object l = embed.lookup( MockComponent.ROLE );
+
+            assertEquals( "not same hashCode for singleton component", l.hashCode(), m.hashCode() );
+
+            assertEquals( "not same hashCode for singleton component", l.hashCode(), m.hashCode() );
+
+            //}
+            embed.stop();
+        }
+        catch ( Exception e )
+        {
+            System.out.println( "msg  = " + e.getMessage() );
+            e.printStackTrace();
+            fail( e.getMessage() );
+        }
+    }
+}
