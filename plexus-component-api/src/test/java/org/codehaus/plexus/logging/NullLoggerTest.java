2d6dea6674ff966349b5c489effa452aeb445450
handyande <handyande@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-02-19 19:55:50 +0000

    Add a small test for the NullLogger

---
+++ plexus-component-api/src/test/java/org/codehaus/plexus/logging/NullLoggerTest.java
@@ -0,0 +1,45 @@
+package org.codehaus.plexus.logging;
+
+import junit.framework.TestCase;
+
+/**
+ * A quick test to make sure the NullLogger is working when we fail to configure our logging.
+ *
+ * @author Andrew Williams
+ * @since 1.0-alpha-18
+ * @version $Id$
+ */
+public class NullLoggerTest
+    extends TestCase
+{
+    private SimpleLogEnabled testClass;
+
+    public void setUp()
+    {
+        testClass = new SimpleLogEnabled();
+    }
+
+    public void testSingleNullLogger()
+    {
+        assertNotNull( testClass.getLogger() );
+    }
+
+    public void testMultiNullLogger()
+    {
+        AbstractLogEnabled enabled = new AnotherLogEnabled();
+
+        assertNotSame( testClass.getLogger(), enabled.getLogger() );
+    }
+}
+
+class SimpleLogEnabled
+    extends AbstractLogEnabled
+{
+
+}
+
+class AnotherLogEnabled
+    extends AbstractLogEnabled
+{
+
+}
