d80dfe3b117e1e6bd0fbb199d5c1cc9304038489
carlos <carlos@30dfa635-7bf8-0310-9608-e41f2d776689>
2007-05-22 01:14:18 +0000

    [PLX-340] LogEnablePhase and LogDisablePhase don't use the correct logging manager

--- src/main/java/org/codehaus/plexus/personality/plexus/lifecycle/phase/LogDisablePhase.java
+++ src/main/java/org/codehaus/plexus/personality/plexus/lifecycle/phase/LogDisablePhase.java
@@ -1,7 +1,7 @@
 package org.codehaus.plexus.personality.plexus.lifecycle.phase;
 
 /*
- * Copyright 2001-2006 Codehaus Foundation.
+ * Copyright 2001-2007 Codehaus Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,9 @@ package org.codehaus.plexus.personality.plexus.lifecycle.phase;
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.codehaus.plexus.component.manager.ComponentManager;
 import org.codehaus.plexus.component.repository.ComponentDescriptor;
-import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.codehaus.plexus.lifecycle.phase.AbstractPhase;
 import org.codehaus.plexus.logging.LogEnabled;
 import org.codehaus.plexus.logging.LoggerManager;
-
-import java.util.Map;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
@@ -38,18 +35,7 @@ public class LogDisablePhase
     {
         if ( object instanceof LogEnabled )
         {
-            LoggerManager loggerManager;
-            try
-            {
-                /* as we do not know what logger role hint has been configured pull the first logger we find. Andy
-                 * TODO - figure how to make this more determanistic? */
-                Map loggers = componentManager.getContainer().lookupMap( LoggerManager.ROLE, lookupRealm );
-                loggerManager = (LoggerManager) loggers.get( loggers.keySet().iterator().next() );
-            }
-            catch ( ComponentLookupException e )
-            {
-                throw new PhaseExecutionException( "Unable to locate logger manager", e );
-            }
+            LoggerManager loggerManager = componentManager.getContainer().getLoggerManager();
 
             ComponentDescriptor descriptor = componentManager.getComponentDescriptor();
             loggerManager.returnComponentLogger( descriptor.getRole(), descriptor.getRoleHint() );
