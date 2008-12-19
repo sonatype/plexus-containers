095c3abedf74224378547f5b81e3bcac3a856aa2
igorife <igorife@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-12-19 17:05:02 +0000

    plexus test case looses context customization

--- plexus-container-default/src/main/java/org/codehaus/plexus/PlexusTestCase.java
+++ plexus-container-default/src/main/java/org/codehaus/plexus/PlexusTestCase.java
@@ -52,13 +52,13 @@ public abstract class PlexusTestCase
         // Context Setup
         // ----------------------------------------------------------------------------
 
-        Map<Object, Object> context = new HashMap<Object, Object>();
+        DefaultContext context = new DefaultContext();
 
         context.put( "basedir", getBasedir() );
 
-        customizeContext( new DefaultContext( context ) );
+        customizeContext( context );
 
-        boolean hasPlexusHome = context.containsKey( "plexus.home" );
+        boolean hasPlexusHome = context.contains( "plexus.home" );
 
         System.out.println( context );
         
@@ -85,7 +85,7 @@ public abstract class PlexusTestCase
 
         ContainerConfiguration containerConfiguration = new DefaultContainerConfiguration()
             .setName( "test" )
-            .setContext( context );
+            .setContext( context.getContextData() );
 
         if ( config != null )
         {
---
+++ plexus-container-default/src/test/java/org/codehaus/plexus/component/PlexusTestCaseTest.java
@@ -0,0 +1,28 @@
+package org.codehaus.plexus.component;
+
+import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.context.Context;
+import org.codehaus.plexus.context.ContextException;
+
+public class PlexusTestCaseTest
+    extends PlexusTestCase
+{
+    
+    private static final String CUSTOM_PROPERTY = "custom.property";
+    private static final String CUSTOM_VALUE = "custom.value";
+
+    @Override
+    protected void customizeContext( Context context )
+    {
+        super.customizeContext( context );
+        
+        context.put( CUSTOM_PROPERTY, CUSTOM_VALUE );
+    }
+    
+    public void testCustomizeContext() throws ContextException
+    {
+        String value = (String) getContainer().getContext().get( CUSTOM_PROPERTY );
+        
+        assertEquals( CUSTOM_VALUE, value );
+    }
+}
